3cd5ed5116f29f471a4ff2b3e3d5fa145fc3df32
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2024-01-30 19:46:53 -0300

    Change fixes

--- compile.sh
+++ compile.sh
@@ -17,24 +17,31 @@ function should_compile() {
     echo 0
 }
 
-if [ "$(should_compile "stdafx.hpp" "build/stdafx.hpp.pch")" -ne '0' ]; then
-    clang++ -x c++-header -std=$STD -o build/stdafx.hpp.pch stdafx.hpp]
+CHANGED=0
+
+if [ "$(should_compile "stdafx.hpp" "build/stdafx.hpp.pch")" -ne '0' ] || [ $CHANGED -ne 0 ]; then
+    clang++ -x c++-header -std=$STD -o build/stdafx.hpp.pch stdafx.hpp
+    CHANGED=1
 fi
 
-if [ "$(should_compile "CConfig.cpp" "build/CConfig.cpp.pch")" -ne '0' ]; then
+if [ "$(should_compile "CConfig.cpp" "build/CConfig.cpp.pch")" -ne '0' ] || [ $CHANGED -ne 0 ]; then
     clang++ -include-pch build/stdafx.hpp.pch -x c++-header -std=$STD -o build/CConfig.cpp.pch CConfig.cpp
+    CHANGED=1
 fi
 
-if [ "$(should_compile "ChronoUtils.cpp" "build/ChronoUtils.cpp.pch")" -ne '0' ]; then
+if [ "$(should_compile "ChronoUtils.cpp" "build/ChronoUtils.cpp.pch")" -ne '0' ] || [ $CHANGED -ne 0 ]; then
     clang++ -include-pch build/stdafx.hpp.pch -x c++-header -std=$STD -o build/ChronoUtils.cpp.pch ChronoUtils.cpp
+    CHANGED=1
 fi
 
-if [ "$(should_compile "A.cpp" "build/A.cpp.pch")" -ne '0' ]; then
+if [ "$(should_compile "A.cpp" "build/A.cpp.pch")" -ne '0' ] || [ $CHANGED -ne 0 ]; then
     clang++ -include-pch build/stdafx.hpp.pch -include-pch build/ChronoUtils.cpp.pch -x c++-header -std=$STD -o build/A.cpp.pch A.cpp
+    CHANGED=1
 fi
 
-if [ "$(should_compile "B.cpp" "build/B.cpp.pch")" -ne '0' ]; then
+if [ "$(should_compile "B.cpp" "build/B.cpp.pch")" -ne '0' ] || [ $CHANGED -ne 0 ]; then
     clang++ -include-pch build/stdafx.hpp.pch -include-pch build/ChronoUtils.cpp.pch -x c++-header -std=$STD -o build/B.cpp.pch B.cpp
+    CHANGED=1
 fi
 
 clang++ -std=$STD -include-pch build/stdafx.hpp.pch -include-pch build/ChronoUtils.cpp.pch -include-pch build/A.hpp.pch -include-pch build/B.hpp.pch -include-pch build/CConfig.cpp.pch main.cpp
